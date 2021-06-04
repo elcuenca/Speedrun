27e117464f7adbd3ef8da8a704afec6b2c3bd087
ProfessorEL <elmercuenca74@gmail.com>
2021-06-05 02:49:05 +0800

    Create categories and products form for admin page

---
+++ admin/categories.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<?php
+  include('admin_partials/head.php');
+?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php
+    include('admin_partials/header.php');
+    include('admin_partials/sidecolumn.php');
+  ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Control panel</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+    <div class="row">
+      <div class="col-sm-3">
+      </div>
+
+      <!--Form Starts-->
+      <div class="col-sm-6">
+      <h1>Categories</h1>
+      <form role="form">
+                <div class="box-body">
+                  <div class="form-group">
+                    <label for="category">Category</label>
+                    <input type="text" class="form-control" id="category" placeholder="Enter a new category">
+                  </div>
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+      </div>
+      <!-- Form Ends -->
+      <div class="col-sm-3">
+      </div>
+    </div>  
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php
+    include('admin_partials/footer.php');
+  ?>
+</body>
+</html>
