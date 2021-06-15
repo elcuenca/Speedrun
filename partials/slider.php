c78475b0f9192448ccb705a1e7caaadbd2518847
ProfessorEL <elmercuenca74@gmail.com>
2021-06-15 21:42:34 +0800

    Fixed top bar for all pages

--- partials/slider.php
+++ partials/slider.php
@@ -6,13 +6,13 @@
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
 								<span class="ltext-101 cl2 respon2">
-									Category 1
+									RUNNING SHOES COLLECTION
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									Subtitle for category 1
+									Nike's Men <br>Revolution <br>5
 								</h2>
 							</div>
 								
