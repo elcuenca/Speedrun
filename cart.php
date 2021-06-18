8e6a40c8e0a98e9763a6221b099fe87ccea8f72c
ProfessorEL <elmercuenca74@gmail.com>
2021-06-19 01:23:57 +0800

    Modified several lines to add Update button for cart

--- cart.php
+++ cart.php
@@ -40,24 +40,24 @@
 							<table class="table-shopping-cart">
 								<tr class="table_head">
 									<th class="column-1">Action</th>
-									<th class="column-2"></th>
+									<th class="column-2">Name</th>
 									<th class="column-3">Price</th>
 									<th class="column-4">Quantity</th>
-									<th class="column-5">Total</th>
+									<th class="column-5">Update</th>
 								</tr>
 
 								<?php
 									if (isset($_SESSION['cart'])) {
 										$subtotal=0;
 										foreach ($_SESSION['cart'] as $key => $value) { 
-										$subtotal=$subtotal+$value['item_price'];
+										
 
 								?>	
 								<tr class="table_row">
 									<td class="column-1">
-										<div class="">
+										<div>
 											<form action="cart_remove.php" method="POST">
-											<button class="btn btn-sm btn-outline-danger" name="remove">Remove</button>
+											<button class="btn btn-sm btn-light" name="remove">Remove</button>
 											<input type="hidden" name="item_name" value="<?php echo $value['item_name'] ?>">
 											</form>
 										</div>
@@ -65,19 +65,23 @@
 									<td class="column-2"><?php echo $value['item_name'] ?></td>
 									<td class="column-3">&#8369 <?php echo $value['item_price'] ?></td>
 									<td class="column-4">
+										<form action="cart_update.php" method="POST">
 										<div class="wrap-num-product flex-w m-l-auto m-r-0">
 											<div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
 												<i class="fs-16 zmdi zmdi-minus"></i>
 											</div>
 
-											<input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product1" value="<?php echo $value['quantity'] ?>">
+											<input name="quantity"class="mtext-104 cl3 txt-center num-product" type="number" value="<?php echo $value['quantity'] ?>">
 
 											<div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
 												<i class="fs-16 zmdi zmdi-plus"></i>
 											</div>
 										</div>
 									</td>
-									<td class="column-5">&#8369 <?php echo $value['item_price'] ?></td>
+								
+									<td class="column-5"><button class="btn btn-sm btn-secondary" name="update">Update</button>
+											<input type="hidden" name="item_name" value="<?php echo $value['item_name'] ?>">
+											</form></td>
 								</tr>
 								<?php }
 
@@ -86,19 +90,6 @@
 							</table>
 						</div>
 
-						<div class="flex-w flex-sb-m bor15 p-t-18 p-b-15 p-lr-40 p-lr-15-sm">
-							<div class="flex-w flex-m m-r-20 m-tb-5">
-								<input class="stext-104 cl2 plh4 size-117 bor13 p-lr-20 m-r-10 m-tb-5" type="text" name="coupon" placeholder="Coupon Code">
-									
-								<div class="flex-c-m stext-101 cl2 size-118 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-5">
-									Apply coupon
-								</div>
-							</div>
-
-							<div class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
-								Update Cart
-							</div>
-						</div>
 					</div>
 				</div>
 
