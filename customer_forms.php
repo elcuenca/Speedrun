5773c55e17e0772098906e8133049eb2875d49b1
ProfessorEL <elmercuenca74@gmail.com>
2021-06-21 01:11:27 +0800

    Added a name tag for Login Button at Line45

--- customer_forms.php
+++ customer_forms.php
@@ -42,7 +42,7 @@
 							
 						</div>
 
-						<button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
+						<button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer" name="login">
 							Login
 						</button>
 					</form>
