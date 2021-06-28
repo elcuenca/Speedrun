695671e226ca1c95904b9bc20969242174a37cfc
ProfessorEL <elmercuenca74@gmail.com>
2021-06-28 17:43:01 +0800

    Change temporary names to proper terms

--- partials/slider.php
+++ partials/slider.php
@@ -30,13 +30,13 @@
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
 								<span class="ltext-101 cl2 respon2">
-									Category 2
+									GET THE HOTTEST SHOES
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									Subtitle for category 2
+									Trending <br> Shoes
 								</h2>
 							</div>
 								
@@ -54,13 +54,13 @@
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
 								<span class="ltext-101 cl2 respon2">
-									Category 3
+									LIMITED STOCKS ONLY!
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									Subtitle for category 3
+									Nike Air Max <br> 90 Essential
 								</h2>
 							</div>
 								
